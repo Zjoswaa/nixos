946ccd3090a8af87eea7ffbfd5354b53fdb72bbc
Zjoswaa <joshuavdjagt@gmail.com>
2026-04-17 12:44:37 +0200

    Update flake.lock and remove default search engine from Firefox configuration

--- modules/browser/default.nix
+++ modules/browser/default.nix
@@ -8,7 +8,6 @@
         id = 0;
         name = "default";
         isDefault = true;
-        search.default = "google";
         extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
           ublock-origin
           darkreader
